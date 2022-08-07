a3c667272ab3134bb1789f2ca06a77762334ab93
Fpsska <fpsska1337@gmail.com>
2022-08-07 18:25:44 +0300

    feat: add handle of onError func for card__image HTML-el & add author field in package.json configuration

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from "react";
-import placeholder from "../../assets/images/no-photo.jpg"
+import placeholderIMG from "../../assets/images/no-photo.jpg"
 import "./card.scss"
 
 const Card = ({ name, price, preview, manufacturer, brand }) => {
-  const [image, setImage] = useState(placeholder)
+  const [image, setImage] = useState(placeholderIMG)
 
   useEffect(() => {
     if (preview.length > 15) {
@@ -13,7 +13,15 @@ const Card = ({ name, price, preview, manufacturer, brand }) => {
 
   return (
     <div className="card" data-manufacturer={manufacturer} data-brand={brand}>
-      <img className="card__image" src={image} alt="card" />
+      <img
+        className="card__image"
+        src={image}
+        alt="card"
+        onError={e => {
+          e.target.src = placeholderIMG;
+          e.onerror = null;
+        }}
+      />
       <ul className="card__information information">
         <li className="information__name">name: {name}</li>
         <li className="information__price">price: {`${price}$`}</li>
